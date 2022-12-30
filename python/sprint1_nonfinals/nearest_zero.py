9404512274b2c6e207eed2cbab6d4b5edaba3c73
flash <okslen@gmail.com>
2022-12-30 20:18:35 +0300

    nearest zero different def

--- python/sprint1_nonfinals/nearest_zero.py
+++ python/sprint1_nonfinals/nearest_zero.py
@@ -1,9 +1,12 @@
 from typing import List, Tuple
 
 
+def get_zero_indexes(length: int, numbers: Tuple[int]) -> List[int]:
+    return [index for index in range(length) if not numbers[index]]
+
+
 def get_nearest_zero(length: int, numbers: Tuple[int]) -> List[int]:
-    nearest_zero = [0] * length
-    return nearest_zero
+    return get_zero_indexes(length, numbers)
 
 
 def read_input() -> Tuple[int, Tuple[int]]:
