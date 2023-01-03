0b77f04202171ea41611707cb15a2355688d5b60
flash <okslen@gmail.com>
2023-01-03 14:19:49 +0300

    del node by index

--- python/sprint2/D/code.py
+++ python/sprint2/D/code.py
@@ -1,18 +1,24 @@
 # ! change LOCAL to False before submitting !
 # set LOCAL to True for local testing
 
-LOCAL = True
+LOCAL = False
 
 if LOCAL:
-    class Node:  
-        def __init__(self, value, next_item=None):  
-            self.value = value  
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
             self.next_item = next_item
 
+
 def solution(node, elem):
-    # Your code
-    # ヽ(´▽`)/
-    pass
+    index = 0
+    while node:
+        if node.value == elem:
+            return index
+        node = node.next_item
+        index += 1
+    return -1
+
 
 def test():
     node3 = Node("node3", None)
@@ -21,6 +27,7 @@ def test():
     node0 = Node("node0", node1)
     idx = solution(node0, "node2")
     assert idx == 2
+
 
 if __name__ == '__main__':
     test()
