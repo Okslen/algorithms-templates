87862e7ebba80dd2a8ceec246e4f3fd082cae8b1
Игорь Дроздовский <okslen@gmail.com>
2023-01-16 20:02:00 +0300

    begin spr14

--- python/sprint17/L/code.py
+++ python/sprint17/L/code.py
@@ -1,4 +1,11 @@
-from typing import List
+from typing import List, Tuple
 
-def two_bicycle(arr, price, left, right):
+def two_bicycle(arr: Tuple[int, ...], price: int, left, right):
     pass
+
+
+def read_input() -> Tuple[Tuple[int, ...], int]:
+    days_count = input()
+    arr = tuple(map(int, input().strip().split()))
+    price = int(input())
+    return arr, price
